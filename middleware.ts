a4c0aadc6925f5cccb61395696709c0e7a3eee23
Darren Leung <leungyatlong1208@gmail.com>
2025-08-30 00:31:18 +0800

    feat: install next-intl and configure basic i18n setup

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import createMiddleware from 'next-intl/middleware'
+
+export default createMiddleware({
+  // Supported locales
+  locales: ['en', 'zh-HK'],
+  
+  // Default locale
+  defaultLocale: 'en',
+  
+  // Locale detection strategy
+  localeDetection: true,
+  
+  // Locale prefix strategy
+  localePrefix: 'as-needed'
+})
+
+export const config = {
+  // Match all pathnames except for
+  // - API routes
+  // - Static files
+  // - Internal Next.js files
+  matcher: ['/((?!api|_next|.*\\..*).*)']
+}
